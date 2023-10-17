c707f7c9d58e260f903dee1dfe23a45e4291e56c
Nauris Vasilevskis <nafris@inbox.lv>
2023-10-17 12:34:15 +0100

    write text feature added

--- screen.c
+++ screen.c
@@ -3,6 +3,8 @@
 #include "font.h"
 
 unsigned char buffer[128][8];
+int cursorX = 0;
+int cursorY = 0;
 
 int get_y8(int y64){
 	return y64 / 8;
@@ -32,6 +34,7 @@ void print_buffer(void){
                 }
         }
 }
+
 
 void draw_dot(int x128, int y64){
 	int y8 = get_y8(y64);
@@ -66,22 +69,39 @@ void draw_rect(int x1, int y1, int x2, int y2){
 	draw_v_line(y1, y2, x2);
 }
 
-void draw_letter(int x128, int y8, char letter[8]){
+void draw_letter(char letter[8]){
 	for (int i = 0; i < 8; i++){
-		buffer[x128 + i][y8] = letter[i];
+		buffer[cursorX * 8 + i][cursorY] = letter[i];
+	}
+	cursorX++;
+}
+
+void write_text(char text[16]){
+	for (int i = 0; i < strlen(text); i++){
+		draw_letter(font[text[i]]);
 	}
 }
 
 void main(void){
 	//unsigned char buffer[128][8];
 	memset(&buffer, 0, sizeof(buffer));
-	draw_dot(127, 63);
+	//draw_dot(127, 63);
 	//draw_h_line(10, 20, 5);
 	//draw_v_line(40,60,20);
-	draw_rect(10,10,20,20);
+	//draw_rect(10,10,20,20);
 
 	char letter[8];
-	memcpy(&letter, font[100], sizeof(letter));
-	draw_letter(20, 3, letter);
+	//memcpy(&letter, font[100], sizeof(letter));
+	//draw_letter(letter);
+	//memcpy(&letter, font[105], sizeof(letter));
+	//draw_letter(letter);
+	//print_buffer();
+	write_text("Chemtrails");
+	cursorY++;
+	cursorX = 0;
+	write_text("  are");
+	cursorY++;
+	cursorX = 0;
+	write_text("the GOAT");
 	print_buffer();
 }
