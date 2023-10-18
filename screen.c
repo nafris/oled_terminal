407c3a6b7027919a2afc37144c162b57df963b52
Nauris Vasilevskis <nafris@inbox.lv>
2023-10-18 07:05:22 +0100

    moved some functions to macros

--- screen.c
+++ screen.c
@@ -2,46 +2,37 @@
 #include <string.h>
 #include "font.h"
 
+#define GET_Y8(y) (y / 8)
+#define GET_B8(y) (y % 8)
+#define GET_BIT(data, b) ((data >> b) % 2)
+
+
 unsigned char buffer[128][8];
 int cursorX = 0;
 int cursorY = 0;
-
-int get_y8(int y64){
-	return y64 / 8;
-}
-
-int get_b8(int y64){
-	return y64 % 8;
-}
-
-int get_bit(unsigned char data, int bit){
-	data = data >> bit;
-	return data % 2;
-}
 
 void print_buffer(void){
         for (int y = 0; y < 8; y++){
                 for (int b = 0; b < 8; b++){
                         for(int x = 0; x < 128; x++){
                                 //printf("%d", get_bit(buffer[x][y], b));
-				printf("%c", get_bit(buffer[x][y], b) ? '%' :  ' ');
+				printf("%c", GET_BIT(buffer[x][y], b) ? '%' :  ' ');
                         }
                         printf("\n");
                 }
         }
 }
 
-
 void draw_dot(int x128, int y64){
-	int y8 = get_y8(y64);
-	int b8 = get_b8(y64);
+	int y8 = GET_Y8(y64);
+	int b8 = GET_B8(y64);
 	buffer[x128][y8] |= 1 << b8;
 }
 
 void draw_h_line(int x1, int x2, int y64){
 	//need to add fix if x2 < x1
-	int y8 = get_y8(y64);
-	int b8 = get_b8(y64);
+	int y8 = GET_Y8(y64);
+	int b8 = GET_B8(y64);
 	for(int i = x1; i <= x2; i++){
 		buffer[i][y8] |= 1 << b8;
 	}
@@ -52,8 +43,8 @@ void draw_v_line(int y1, int y2, int x128){
 	int y8;
 	int b8;
 	for (int i = y1; i <= y2; i++){
-		y8 = get_y8(i);
-		b8 = get_b8(i);
+		y8 = GET_Y8(i);
+		b8 = GET_B8(i);
 		buffer[x128][y8] |= 1 << b8;
 	}
 }
@@ -70,8 +61,8 @@ void draw_line(int x1, int y1, int x2, int y2){
 	k = 1024 * (y2 - y1) / (x2 - x1);
 	for (int i = x1; i <= x2; i++){
 		y = (i - x1) * k / 1024 + y1;
-		y8 = get_y8(y);
-		b8 = get_b8(y);
+		y8 = GET_Y8(y);
+		b8 = GET_B8(y);
 		buffer[i][y8] |= 1 << b8;
 	}
 }
